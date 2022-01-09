85798d459a1ffcdca4af6e3862a0a29299204672
Liz Lautenslager <liz.lautenslager@gmail.com>
2022-01-08 21:57:22 -0500

    update

--- app.js
+++ app.js
@@ -1,177 +1,108 @@
-// Function for change on dropdown menu
-function optionChanged(selectedID){
+function optionChanged(idNum) {
+   console.log(idNum);
 
-    // Check if value is selected in dropdown
-    console.log(selectedID);
- 
-    // Read the json file for the data
-    d3.json("data/samples.json").then((data) => {
- 
-   //  console.log(data);
- 
-    // Clears dropdown
-    d3.select("#selDataset").html("");   
-    
-    // Select the metadata array and for each item append the item ID and adds ID to dropdown
-    data.metadata.forEach(item =>
-         {
-          // console.log(item.id);
-         d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
-         });
-    // Selected value is passed
-    d3.select("#selDataset").node().value = selectedID;
-    
-    // Filter Metadata for selected ID from dropdown
-    const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
-       // {
-       //    console.log("------------------------")
-       //    console.log(item);
-       //    console.log(item.id);
-          
-       // });
-    // Check the metadata loaded for the selected ID
-    console.log(idMetadata);
-    
-    const panelDisplay = d3.select("#sample-metadata");
-    panelDisplay.html("");
-    Object.entries(idMetadata[0]).forEach(item=> 
-       {
-          // console.log(item);
-          panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
-       });
- 
-    // BAR CHART
- 
-    // Filter sample array data for the selected ID
-    const idSample = data.samples.filter(item => parseInt(item.id) == selectedID);
-    
-    // // Check values
-    // console.log(typeof parseInt(item.id));
-    // console.log(idSample[0].sample_values);  
-    // console.log(idSample[0].otu_ids);  
-    // console.log(idSample[0].otu_labels);  
-    
-    // Slice top 10 sample values
-    var sampleValue = idSample[0].sample_values.slice(0,10);
-    sampleValue= sampleValue.reverse();
-    var otuID = idSample[0].otu_ids.slice(0,10);
-    otuID = otuID.reverse();
-    var otuLabels = idSample[0].otu_labels
-    otuLabels = otuLabels.reverse();
- 
-    // // Check values
-    //  console.log(sampleValue);
-    //  console.log(otuID);
-    //  console.log(otuLabels);
- 
-    // Y axis of bar chart
-    const yAxis = otuID.map(item => 'OTU' + " " + item);
-       // console.log(yAxis);
-    
-    // Define the layout and trace object, edit color and orientation
-       const trace = {
-       y: yAxis,
-       x: sampleValue,
-       type: 'bar',
-       orientation: "h",
-       text:  otuLabels,
-       marker: {
-          color: 'rgb(154, 140, 152)',
-          line: {
-             width: 3
-         }
-        }
-       },
-       layout = {
-       title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
-       xaxis: {title: 'Number of Samples Collected'},
-       yaxis: {title: 'OTU ID'}
-       };
- 
-       // Plot using Plotly
-       Plotly.newPlot('bar', [trace], layout,  {responsive: true});    
-       
- // BUBBLE CHART
- 
- // Remove Sample value and otuID from individual
- var sampleValue1 =idSample[0].sample_values;
- var otuID1= idSample[0].otu_ids;
- 
- // Define the layout and trace object, edit color and orientation
- const trace1 = {
-    x: otuID1,
-    y: sampleValue1,
-    mode: 'markers',
-    marker: {
-      color: otuID1,
-      
-      size: sampleValue1
-    }
-  },
- 
-  layout1 = {
-    title: '<b>Bubble Chart For Each Sample</b>',
-    xaxis: {title: 'OTU ID'},
-    yaxis: {title: 'Number of Samples Collected'},
-    showlegend: false,
-    height: 800,
-    width: 1800
-    };
-    
- // Plot using Plotly
- Plotly.newPlot('bubble', [trace1], layout1);
- 
- // BONUS: GAUGE CHART
+d3.json("data/samples.json").then(function(data) {
 
- // Gauge Chart to plot weekly washing frequency 
- const guageDisplay = d3.select("#gauge");
- guageDisplay.html(""); 
- const washFreq = idMetadata[0].wfreq;
- 
- const guageData = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: washFreq,
-      title: { text: "<b>Belly Button Washing Frequency </b><br> (Scrubs Per Week)" },
-      type: "indicator",
-      mode: "gauge+number",     
-       gauge: {
-       axis: { range: [0,9] },
-       bar: { color: "#f2e9e4" },
-       steps: [
-          { range: [0, 1], color: "#e5d5d0" },
-          { range: [1, 2], color: "#dbc7c2" },
-          { range: [2, 3], color: "#d2b9b4" },
-          { range: [3, 4], color: "#c9ada7" },
-          { range: [4, 5], color: "#ac9899" },
-          { range: [5, 6], color: "#8a7e88" },
-          { range: [6, 7], color: "#7d7482" },
-          { range: [7, 8], color: "#706a7b" },
-          { range: [8, 9], color: "#4a4e69" }
-                
-        ],
-       threshold: {
-          value: washFreq
-        }
+   metadata = Object.values(data.metadata.filter(function(findMeta){
+      return findMeta.id.toString() == idNum;
+   }));
+   metadata = metadata[0];
+   console.log(metadata);
+
+   d3.select("#sample-metadata").html("");
+   Object.entries(metadata).forEach(([key, value]) => {
+      d3.select("#sample-metadata").append("p").text(`${key}: ${value}`);
+   });
+
+   samples = Object.values(data.samples.filter(function(findsample) {
+      return findsample.id.tostring() == idNum;
+   }));
+   samples = samples[0];
+   console.log(samples);
+
+   samples.sample_values.sort(function sortFunction(a, b) {
+      return b - a;
+   });
+
+   sample_values = samples.sample_values.slice(0, 10).reverse();
+   otu_ids = samples.otu_ids.slice(0, 10).reverse();
+   otu_lables = samples.otu_lables.slice(0, 10).reverse();
+   console.log(sample_values, otu_ids, otu_lables);
+
+   var barGraph = [{
+      x: sample_values,
+      y: otu_ids.map(function(otuID) {return `OTU ${otuID}`;}),
+      type: "bar",
+      orientation: "h",
+      hoverinfo: "text",
+      hovertext: otu_lables
+   }];
+   var barLayout = {
+      title:"<b>Top 10 OTUs</b>"
+   }
+
+   Plotly.newPlot("bar", barGraph, barLayout);
+
+   var bubbleChart = [{
+      x: samples.otu_ids,
+      y: samples.sample_values,
+      text: samples.otu_lables,
+      mode: "markers",
+      marker: {
+         size: samples.sample_values,
+         color: samples.otu_ids
       }
-    }
-  ]; 
-  const gaugeLayout = {  width: 600, 
-                   height: 400, 
-                   margin: { t: 0, b: 0 }, 
-                    };
- 
- // Plot using Plotly
-  Plotly.newPlot('gauge', guageData, gaugeLayout); 
- 
- });
- }
- 
- // Initial test starts at ID 940
- optionChanged(940);
- 
- // Event on change takes the value and calls the function during dropdown selection
- d3.select("#selDataset").on('change',() => {
- optionChanged(d3.event.target.value);
- 
- });
+   }];
+
+   var layout = {
+      xaxis: {title: "OTU ID"},
+      title: "<b>Values of All OTUs</b>"
+   };
+
+   Plotly.newPlot("buble", bubbleChart, layout);
+
+   var gaugeChart = [{
+      domain: {
+         x: [0, 1],
+         y: [0, 1]
+      },
+      value:metadata.wfreq,
+      title: {
+         text: "<Span style='font-size: 20px;'><b>Belly Button Washing Frequency</b></span><br> Scrubs per Week"
+      },
+      type:"indicator",
+      mode: "gauge+number",
+      gauge: {
+         axis:{range: [null, 9]},
+         steps: [
+            {range: [0, 1], color: "rgb(238, 240, 235)"},
+            {range: [1, 2], color: "rgb(205, 217, 189)"},
+            {range: [2, 3], color: "rgb(186, 204, 161)"},
+            {range: [3, 4], color: "rgb(168, 191, 136)"},
+            {range: [4, 5], color: "rgb(152, 181, 112)"},
+            {range: [5, 6], color: "rgb(136, 171, 89)"},
+            {range: [6, 7], color: "rgb(123, 163, 70)"},
+            {range: [7, 8], color: "rgb(109, 154, 49)"},
+            {range: [8, 9], color: "rgb( 96, 143, 34)"},
+         ]
+      }
+   }];
+
+   Plotly.newPlot("gauge", gaugeChart);
+
+
+});
+}
+
+d3.json("samples.json").then(function(data) {
+   console.log(data);
+
+   var dropdown = d3.select("#selDataset");
+
+   data.names.forEach(function(name) {
+      dropdownMenu.append("option").text(name);
+   });
+
+   optionChanged("940");
+
+});
